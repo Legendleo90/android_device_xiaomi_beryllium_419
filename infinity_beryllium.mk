8eeb2aec3a815f272a539479c86b2f2e0037487d
Akshat <Legendleo90@users.noreply.github.com>
2025-08-31 18:58:45 +0530

    beryllium: Bringup @ProjectInfinity-X

--- infinity_beryllium.mk
+++ infinity_beryllium.mk
@@ -1,16 +1,21 @@
 #
-# Copyright (C) 2018-2022 The LineageOS Project
+# Copyright (C) 2018-2025 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Infinity-X stuff.
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
+
+# Infinity-X Specific Flags
+TARGET_BOOT_ANIMATION_RES := 1080
+INFINITY_BUILD_TYPE := OFFICIAL
+INFINITY_MAINTAINER := "Akshat"
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_beryllium
+PRODUCT_NAME := infinity_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
