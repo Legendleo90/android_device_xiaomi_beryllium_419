89f7013dc7d74a07330b2b51a60ff9b02f0019f7
Akshat <Legendleo90@users.noreply.github.com>
2026-02-02 23:07:33 +0530

    beryllium: Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,7 @@ rm -rf vendor/xiaomi/beryllium
 rm -rf vendor/xiaomi/sdm845-common
 rm -rf kernel/xiaomi/sdm845
 rm -rf hardware/xiaomi
+rm -rf hardware/dolby
 rm -rf hardware/qcom-caf/sdm845/media
 rm -rf hardware/qcom-caf/sdm845/audio
 rm -rf hardware/qcom-caf/sdm845/display
@@ -17,7 +18,8 @@ git clone https://github.com/Legendleo90/android_device_xiaomi_sdm845-common_419
 git clone https://github.com/Legendleo90/android_vendor_xiaomi_beryllium_419.git vendor/xiaomi/beryllium
 git clone https://github.com/Legendleo90/android_vendor_xiaomi_sdm845-common_419.git vendor/xiaomi/sdm845-common
 git clone https://github.com/Legendleo90/android_kernel_xiaomi_sdm845_419.git kernel/xiaomi/sdm845
-git clone https://github.com/Legendleo90/hardware_xiaomi.git hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git hardware/xiaomi
+git clone https://github.com/Legendleo90/hardware_dolby.git hardware/dolby
 git clone https://github.com/Legendleo90/android_hardware_qcom_media.git -b lineage-23.1-caf-sm8150  hardware/qcom-caf/sdm845/media
 git clone https://github.com/Legendleo90/android_hardware_qcom_audio.git -b lineage-23.1-caf-sm8250 hardware/qcom-caf/sdm845/audio
 git clone https://github.com/Legendleo90/android_hardware_qcom_display.git hardware/qcom-caf/sdm845/display
